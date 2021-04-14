5f2cea0dab898a4ab1abf31de31241f9cf801f4a
Indranil Tiwary <indraniltiwary@hotmail.com>
2021-04-14 22:12:40 +0530

    Changed Copy to Likes

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -19,7 +19,7 @@ const HomePage = () => (
             </div>
             <div className="menu-item">
                 <div className="content">
-                    <h1 className="title">HATS</h1>
+                    <h1 className="title">Likes</h1>
                     <span className="subtitle">SHOP NOW</span>
                 </div>
             </div>
